62eba5ae4aefe68db94de338a03d97dd18294838
deepanm87 <deepan.murugan29@gmail.com>
2025-04-03 16:14:50 -0400

    added canvas setup

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-const activeToolEl = document.getElementById('active-tool');
-const brushColorBtn = document.getElementById('brush-color');
-const brushIcon = document.getElementById('brush');
-const brushSize = document.getElementById('brush-size');
-const brushSlider = document.getElementById('brush-slider');
-const bucketColorBtn = document.getElementById('bucket-color');
-const eraser = document.getElementById('eraser');
-const clearCanvasBtn = document.getElementById('clear-canvas');
-const saveStorageBtn = document.getElementById('save-storage');
-const loadStorageBtn = document.getElementById('load-storage');
-const clearStorageBtn = document.getElementById('clear-storage');
-const downloadBtn = document.getElementById('download');
-const { body } = document;
+const activeToolEl = document.getElementById('active-tool')
+const brushColorBtn = document.getElementById('brush-color')
+const brushIcon = document.getElementById('brush')
+const brushSize = document.getElementById('brush-size')
+const brushSlider = document.getElementById('brush-slider')
+const bucketColorBtn = document.getElementById('bucket-color')
+const eraser = document.getElementById('eraser')
+const clearCanvasBtn = document.getElementById('clear-canvas')
+const saveStorageBtn = document.getElementById('save-storage')
+const loadStorageBtn = document.getElementById('load-storage')
+const clearStorageBtn = document.getElementById('clear-storage')
+const downloadBtn = document.getElementById('download')
+const { body } = document
+const canvas = document.createElement('canvas')
+canvas.id = 'canvas'
+const context = canvas.getContext('2d')
 
-// Global Variables
-
-let currentSize = 10;
-let bucketColor = '#FFFFFF';
-let currentColor = '#A51DAB';
+let currentSize = 10
+let bucketColor = '#FFFFFF'
+let currentColor = '#A51DAB'
 // let isEraser = false;
 // let isMouseDown = false;
 // let drawnArray = [];
@@ -63,11 +64,11 @@ let currentColor = '#A51DAB';
 
 // Create Canvas
 function createCanvas() {
-  // canvas.width = ;
-  // canvas.height = ;
-  // context.fillStyle = ;
-  // context.fillRect();
-  body.appendChild(canvas);
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight - 50
+  context.fillStyle = bucketColor
+  context.fillRect(0, 0, canvas.width, canvas.height)
+  body.appendChild(canvas)
 
 }
 
@@ -112,18 +113,18 @@ function createCanvas() {
 
 // Get Mouse Position
 function getMousePosition(event) {
-  const boundaries = canvas.getBoundingClientRect();
+  const boundaries = canvas.getBoundingClientRect()
   return {
     x: event.clientX - boundaries.left,
     y: event.clientY - boundaries.top,
-  };
+  }
 }
 
 // Mouse Down
 canvas.addEventListener('mousedown', (event) => {
-  isMouseDown = true;
-  const currentPosition = getMousePosition(event);
-  console.log('mouse is clicked', currentPosition);
+  isMouseDown = true
+  const currentPosition = getMousePosition(event)
+  console.log('mouse is clicked', currentPosition)
 //   context.moveTo(currentPosition.x, currentPosition.y);
 //   context.beginPath();
 //   context.lineWidth = currentSize;
@@ -134,8 +135,8 @@ canvas.addEventListener('mousedown', (event) => {
 // Mouse Move
 canvas.addEventListener('mousemove', (event) => {
   if (isMouseDown) {
-    const currentPosition = getMousePosition(event);
-    console.log('mouse is moving', currentPosition);
+    const currentPosition = getMousePosition(event)
+    console.log('mouse is moving', currentPosition)
   //   context.lineTo(currentPosition.x, currentPosition.y);
   //   context.stroke();
   //   storeDrawn(
@@ -196,4 +197,4 @@ canvas.addEventListener('mouseup', () => {
 // brushIcon.addEventListener('click', switchToBrush);
 
 // On Load
-createCanvas();
+createCanvas()
